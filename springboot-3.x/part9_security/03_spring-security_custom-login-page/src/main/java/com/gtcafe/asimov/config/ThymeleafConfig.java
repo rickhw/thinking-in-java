24cd4f306ef8d6b10fe5c4b3652f5af9d5527cd6
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-08 15:27:44 +0800

    update on 20250308-1527, comment:

--- springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/config/ThymeleafConfig.java
+++ springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/config/ThymeleafConfig.java
@@ -1,10 +1,10 @@
 package com.gtcafe.asimov.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
--- springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/controller/MyController.java
+++ springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/controller/MyController.java
@@ -1,52 +1,51 @@
-// package com.gtcafe.asimov.controller;
+package com.gtcafe.asimov.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.gtcafe.asimov.model.Member;
-// import com.gtcafe.asimov.repository.MemberRepository;
+import com.gtcafe.asimov.model.Member;
+import com.gtcafe.asimov.repository.MemberRepository;
 
-// @RestController
-// public class MyController {
+@RestController
+public class MyController {
 
-//     @Autowired
-//     private MemberRepository memberRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
-//      @Autowired
-//     private PasswordEncoder passwordEncoder;
+     @Autowired
+    private PasswordEncoder passwordEncoder;
 
-//     @PostMapping("/members")
-//     public String createMember(@RequestBody Member member) {
-//         var encodedPwd = passwordEncoder.encode(member.getPassword());
-//         member.setPassword(encodedPwd);
-//         member.setId(null);
-//         memberRepository.insert(member);
-//         return member.getId();
-//     }
+    @PostMapping("/members")
+    public String createMember(@RequestBody Member member) {
+        var encodedPwd = passwordEncoder.encode(member.getPassword());
+        member.setPassword(encodedPwd);
+        member.setId(null);
+        memberRepository.insert(member);
+        return member.getId();
+    }
 
-//     @GetMapping("/members")
-//     public List<Member> getMembers() {
-//         return memberRepository.findAll();
-//     }
+    @GetMapping("/members")
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
 
-//     @GetMapping("/selected-courses")
-//     public String selectedCourses() {
-//         return "修課清單";
-//     }
+    @GetMapping("/selected-courses")
+    public String selectedCourses() {
+        return "修課清單";
+    }
 
-//     @GetMapping("/course-feedback")
-//     public String courseFeedback() {
-//         return "課程回饋";
-//     }
+    @GetMapping("/course-feedback")
+    public String courseFeedback() {
+        return "課程回饋";
+    }
 
-//     // @GetMapping("/home")
-//     // public String home() {
-//     //     return "系統首頁";
-//     // }
-// }
+}
