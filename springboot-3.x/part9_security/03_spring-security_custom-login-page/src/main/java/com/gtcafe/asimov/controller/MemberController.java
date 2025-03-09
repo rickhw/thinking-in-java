88d79831b382151f97d41929ec482496ea573946
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-09 09:29:42 +0800

    update on 20250309-0929, comment:

---
+++ springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/controller/MemberController.java
@@ -0,0 +1,38 @@
+package com.gtcafe.asimov.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gtcafe.asimov.model.Member;
+import com.gtcafe.asimov.repository.MemberRepository;
+
+@RestController
+public class MemberController {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+     @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/members")
+    public String createMember(@RequestBody Member member) {
+        var encodedPwd = passwordEncoder.encode(member.getPassword());
+        member.setPassword(encodedPwd);
+        member.setId(null);
+        memberRepository.insert(member);
+        return member.getId();
+    }
+
+    @GetMapping("/members")
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
+
+}
