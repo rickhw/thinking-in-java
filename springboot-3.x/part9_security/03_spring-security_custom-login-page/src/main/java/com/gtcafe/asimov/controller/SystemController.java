25aa136226d6920756c55617c91bf541fda6a469
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-09 10:30:52 +0800

    update on 20250309-1030, comment:

--- springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/controller/SystemController.java
+++ springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/controller/SystemController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/_")
+@Slf4j
 public class SystemController {
 
     @GetMapping("/health")
     public String health() {
+        log.info("Health check");
         return "OK";
     }
 
