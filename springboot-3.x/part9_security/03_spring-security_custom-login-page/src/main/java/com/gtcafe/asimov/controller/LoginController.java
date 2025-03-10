884a1907dad541c666b3c79dce892754f04ac12e
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-10 20:20:39 +0800

    update on 20250310-2020, comment:

--- springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/controller/LoginController.java
+++ springboot-3.x/part9_security/03_spring-security_custom-login-page/src/main/java/com/gtcafe/asimov/controller/LoginController.java
@@ -21,7 +21,10 @@ import lombok.extern.slf4j.Slf4j;
 public class LoginController {
 
     @GetMapping("/login")
-    public String showLoginPage() {
+    public String showLoginPage(HttpServletRequest request) {
+
+        SessionUtil.printSessionInfo(request);
+
         return "login"; // 返回模板名稱
     }
 
