34ecdc5bf8b1d30cf812efad184f473305aea540
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-16 10:07:10 +0800

    update on 20250316-1007, comment:

--- springboot-3.x/part02_advanced-for-go-live/05_security/01_login-form/04_spring-security_session-in-cache/src/main/java/com/gtcafe/asimov/config/RedisConfig.java
+++ springboot-3.x/part02_advanced-for-go-live/05_security/01_login-form/04_spring-security_session-in-cache/src/main/java/com/gtcafe/asimov/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.gtcafe.asimov.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -11,9 +13,15 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableRedisHttpSession
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
