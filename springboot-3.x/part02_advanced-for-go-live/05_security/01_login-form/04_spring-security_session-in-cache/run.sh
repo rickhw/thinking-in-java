bae1e3f064316d8806ec2678396c79c81aa8e439
Rick Hwang <rick_kyhwang@hotmail.com>
2025-03-23 15:15:27 +0800

    update on 20250323-1515, comment:

--- springboot-3.x/part02_advanced-for-go-live/05_security/01_login-form/04_spring-security_session-in-cache/run.sh
+++ springboot-3.x/part02_advanced-for-go-live/05_security/01_login-form/04_spring-security_session-in-cache/run.sh
@@ -3,3 +3,9 @@ export SPRING_DATA_REDIS_HOST=172.31.5.148
 java -jar target/school-0.0.1-SNAPSHOT.jar
 
 SPRING_DATA_MONGODB_URI=mongodb://192.168.2.65:27017/school SPRING_DATA_REDIS_HOST=192.168.2.65 java -jar target/school-0.0.1-SNAPSHOT.jar
+
+
+
+export SPRING_DATA_MONGODB_URI=mongodb://192.168.5.65:27017/school
+export SPRING_DATA_REDIS_HOST=192.168.5.65
+
